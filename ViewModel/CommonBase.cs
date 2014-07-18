5146bd65a52765c189d7d75361145519cb274526
smiles <vissa3@aol.com>
2014-07-18 11:35:17 -0700

    Basic setup for general design

---
+++ ViewModel/CommonBase.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel;
+
+namespace MVVMvideo
+{
+    public class CommonBase : INotifyPropertyChanged 
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void RaisePropertyChanged(string propertyName)
+        {
+            PropertyChangedEventHandler handler = this.PropertyChanged;
+
+            if(handler != null)
+            {
+                PropertyChangedEventArgs args = new PropertyChangedEventArgs(propertyName);
+                handler(this, args);
+            }
+        }
+    }
+}
