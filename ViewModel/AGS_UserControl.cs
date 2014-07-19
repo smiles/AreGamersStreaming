836fe16997017227fa2bd4937d3c15106e0b4f55
smiles <vissa3@aol.com>
2014-07-19 12:49:04 -0700

    data binding all checkboxs to AGS_controls

---
+++ ViewModel/AGS_UserControl.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AreGamersStreaming.ViewModel
+{
+    using Smiles.MvvM.Lib;
+    using Properties;
+
+    public class AGS_UserControl : CommonBase
+    {
+        private bool _IsStartBoot = Settings.Default.Bootatstartup;
+        private bool _IsMinStart = Settings.Default.Minamizeatstart;
+
+        public string AddStream
+        {
+            get;
+            set;
+        }
+
+        public string DelStream
+        {
+            get;
+            set;
+        }
+
+        public bool IsStartBoot
+        {
+            get { return _IsStartBoot; }
+            set
+            {
+                if(_IsStartBoot != value)
+                {
+                    _IsStartBoot = value;
+                    Settings.Default.Bootatstartup = value;
+                    Settings.Default.Save();
+                    RaisePropertyChanged("IsStartBoot");
+                }
+            }
+        }
+
+        public bool IsMinStart
+        {
+            get {return _IsMinStart; }
+            set
+            {
+                if(_IsMinStart != value)
+                {
+                    _IsMinStart = value;
+                    Settings.Default.Minamizeatstart = value;
+                    Settings.Default.Save();
+                    RaisePropertyChanged("IsMinStart");
+                }
+            }
+        }
+    }
+}
--- ViewModel/CommonBase.cs
+++ ViewModel/CommonBase.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.ComponentModel;
 
-namespace MVVMvideo
+namespace Smiles.MvvM.Lib
 {
     public class CommonBase : INotifyPropertyChanged 
     {
