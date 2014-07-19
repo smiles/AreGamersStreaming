73d4c6117c360b6b33ff4a627eb8dd33cde99da2
smiles <vissa3@aol.com>
2014-07-19 11:44:02 -0700

    Added basic UI controls

---
+++ View/StartControl.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace AreGamersStreaming.View
+{
+    /// <summary>
+    /// Interaction logic for StartControl.xaml
+    /// </summary>
+    public partial class StartControl 
+    {
+        public StartControl()
+        {
+            InitializeComponent();
+        }
+    }
+}
