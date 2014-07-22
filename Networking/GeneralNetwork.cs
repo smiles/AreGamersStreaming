76479769ab7378627024d37896f897fc672bb8fa
smiles <vissa3@aol.com>
2014-07-22 11:08:13 -0700

    Started work on TwitchLogic

---
+++ Networking/GeneralNetwork.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+
+namespace Smiles.Common.Lib
+{
+    public static class GeneralNetwork
+    {
+        public static bool IsNetworkConnected()
+        {
+            return NetworkInterface.GetIsNetworkAvailable();
+        }
+    }
+}
--- Twitch/TwitchLogic.cs
+++ Twitch/TwitchLogic.cs
@@ -5,7 +5,35 @@ using System.Text;
 
 namespace AreGamersStreaming.Twitch
 {
-    class TwitchLogic
+    using Properties;
+
+    public class TwitchLogic
     {
+        private int _HowOftenToCheck = Settings.Default.HowOftenToCheck;
+        private List<TwitchStream> _AllStreams = new List<TwitchStream>();
+        private List<string> _StreamList = new List<string>();
+        private string _TwitchStreamAPI = Settings.Default.TwitchAPIStream;
+
+        public TwitchLogic()
+        {
+
+        }
+
+        public void UpdateHowOftenToCheck()
+        {
+            _HowOftenToCheck = Settings.Default.HowOftenToCheck;
+        }
+
+        private void ConvertListToStreams()
+        {
+            _StreamList = Settings.Default.StreamList.Cast<string>().ToList();
+
+        }
+
+        private string ConvertStreamURLToAPIAddress(string URL)
+        {
+            string streamName = URL.Remove(0,13);
+            return _TwitchStreamAPI += streamName;
+        }
     }
 }
